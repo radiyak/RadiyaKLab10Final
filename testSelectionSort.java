761152f18b1b20a24eaf0a968cf528c94a7e8837
radiya <radiyak@umich.edu>
2021-12-09 00:36:11 -0500

    .

--- testSelectionSort.java
+++ testSelectionSort.java
@@ -6,8 +6,7 @@ public class testSelectionSort {
 public void test() {
 testPositive();
 testNegative();
-testMixed();
-testDuplicates();
+
 }
     public testSelectionSort() {
     }
@@ -60,5 +59,5 @@ testDuplicates();
          }
     }
 
-    
+
 }
